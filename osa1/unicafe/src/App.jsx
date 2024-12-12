e2e6a1adae34a994cb43479e7ea4fb265310fee5
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2024-12-12 13:30:42 +0200

    task 1.6

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text='good' handleClick={() => setGood(good + 1)}/>
+      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)}/>
+      <Button text='bad' handleClick={() => setBad(bad + 1)}/>
+      <h1>statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+export default App
