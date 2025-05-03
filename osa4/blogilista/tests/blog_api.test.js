b31ac3765d294a458a1e0af215832bbc4dc53e2e
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-05-03 13:17:59 +0300

    Task 4.9

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -13,11 +13,17 @@ beforeEach(async () => {
     await Blog.insertMany(helper.initialBlogs)
   })
 
-test.only('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-  
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')  
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
+
+test('returned blog objects do not contain _id field', async () => {
+    const response = await api.get('/api/blogs')
+
+    const containsId = response.body.some(blog => '_id' in blog)
+    assert.strictEqual(containsId, false)
+})
 
 after(async () => {
   await mongoose.connection.close()
