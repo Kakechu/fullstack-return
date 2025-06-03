38896b3fdc64a2853825124387bffa9843b31229
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-06-03 10:08:12 +0300

    Tasks 6.21 - 6.22

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,13 +1,26 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
 
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      const updated = anecdotes.map(a => 
+        a.id !== updatedAnecdote.id ? a : updatedAnecdote
+      )
+      queryClient.setQueryData(['anecdotes'], updated)
+    }
+  })
+
   const handleVote = (anecdote) => {
-    console.log('vote')
+    //console.log('vote')
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
   }
 
   const result = useQuery({
@@ -25,7 +38,7 @@ const App = () => {
     return <div>anecdote service not available due to problems in server</div>
   }
 
-  
+
   const anecdotes = result.data
 
   return (
