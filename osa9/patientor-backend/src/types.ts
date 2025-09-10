1afac578c7896ab7ff71c00b31a270b7e48a5452
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-09-10 12:55:03 +0300

    Task 9.23

--- osa9/patientor-backend/src/types.ts
+++ osa9/patientor-backend/src/types.ts
@@ -13,7 +13,7 @@ export enum Gender {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
+//export interface Entry {}
 
 export interface Patient {
   id: string;
@@ -28,3 +28,47 @@ export interface Patient {
 export type NewPatient = z.infer<typeof newPatientSchema>;
 
 export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
